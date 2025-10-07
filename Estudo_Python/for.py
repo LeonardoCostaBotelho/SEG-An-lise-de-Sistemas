05d24e67ca2fbcb0a3933350a604eb9cb4601f54
LeonardoCostaBotelho <leonardo1da0costa3@gmail.com>
2025-10-06 21:49:08 -0300

    Create for.py

---
+++ Estudo_Python/for.py
@@ -0,0 +1,6 @@
+#-- Exemplo de for (for = repetição com limite)
+for i in range(10):
+    print(i) 
+
+#-- Caso não queria um número anterior, coloque o número inicial e final. Ex:
+#-- for i in range (1, 10)
